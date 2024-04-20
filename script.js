b7c69eab291ddaa4ea039a6d546eb511c60f4771
Premnishok-Muhilan <155619575+Premnishok-Muhilan@users.noreply.github.com>
2024-04-20 12:31:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function foo() {
+  var fName = document.getElementById("First_Name").value;
+  console.log(fName);
+
+  var mName = document.getElementById("Middle_Name").value;
+  console.log(mName);
+
+  var lName = document.getElementById("Last_Name").value;
+  console.log(lName);
+
+  var email = document.getElementById("Email").value;
+  console.log(email);
+
+  var Address = document.getElementById("Address").value;
+  console.log(Address);
+}
